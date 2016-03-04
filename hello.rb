a107a714850a8c1984356d34e85cef42743a2eda
None <shimurayu@pm.nttdata.co.jp>
2016-03-04 04:32:37 +0000

    hello edit

--- hello.rb
+++ hello.rb
@@ -1,10 +1,7 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World!"
-end
-
-get '/aiit' do
-  "Hi, AIIT"
+<h1>SNA—\–ñ‰æ–Ê‚Ö‚æ‚¤‚±‚»<h1>
+<a href="sna_yoyaku.html" target="_blank">—\–ñ‰æ–Ê‚Ö</a>
 end
 
